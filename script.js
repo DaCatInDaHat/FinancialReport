b4adc47263f99ccc2b2341d6eecc9a3b3684b372
dacatindahat <volodymyrkuznetsov87@gmail.com>
2023-01-30 02:48:12 +0200

    push to GitHub

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
 const salaries = {
     Manager: { salary: 1000, tax: "10%" },
     Designer: { salary: 600, tax: "30%" },
-    Artist: { salary: 1500, tax: "15%" }
+    Artist: { salary: 1500, tax: "15%" },
+    TeamLead: { salary: 1000, tax: "99%" },
+    Architect: { salary: 9000, tax: "34%" }
 }
 
 const team = [
     { name: "Misha", specialization: "Manager" },
     { name: "Max", specialization: "Designer" },
     { name: "Vova", specialization: "Designer" },
-    { name: "Leo", specialization: "Artist" }]
+    { name: "Leo", specialization: "Artist" },
+    { name: "Alexander", specialization: "TeamLead" },
+    { name: "Gaudi", specialization: "Architect" },
+    { name: "Koolhas", specialization: "Architect" },
+    { name: "Foster", specialization: "Architect" },
+    { name: "Napoleon", specialization: "General" }]
 
 function calculateTeamFinanceReport(salaries, team) {
-    const specializations = team.map(elem => elem.specialization);
-    const specializationsAndSalariesWithTax = {};
+    const specsQuantity = {};
+    const specsSalary = {};
     const report = {};
     let sum = 0;
 
-    let specializationsQuantity = specializations.reduce((prev, cur) => {
-        prev[cur] = (prev[cur] || 0) + 1;
-        return prev;
-    }, {});
+    const specs = team.map(elem => elem.specialization); //take list of specialization
 
-    Object.entries(salaries).map(item => {
-        const salaryWithTax = Math.trunc((item[1].salary * 100) / (100 - parseInt(item[1].tax)));
-        Object.assign(specializationsAndSalariesWithTax, { [item[0]]: salaryWithTax });
+    //count quantity of specialization
+    for (let spec in specs) {
+        let count = 1;
+        if (specs[spec - 1] === specs[spec]) {
+            count++;
+            specsQuantity[specs[spec]] = count;
+        }
+        else {
+            count = 1;
+            specsQuantity[specs[spec]] = count;
+        }
+    }
+
+    //count salaries with tax
+    Object.entries(salaries).forEach(salary => {
+        const salaryWithTax = Math.trunc((salary[1].salary * 100) / (100 - parseInt(salary[1].tax)));
+        specsSalary[salary[0]] = salaryWithTax;
     });
 
-    Object.entries(specializationsAndSalariesWithTax).map(item => {
-        Object.entries(specializationsQuantity).map(element => {
-            if (item[0] === element[0]) {
-                let result = item[1] * element[1];
-                let keyName = `totalBudget${item[0]}`;
-                Object.assign(report, { [keyName]: result });
-            }
-        })
-    });
+    //create list of specializations and their total salaries
+    for (let item in specsSalary) {
+        report[`totalBudget${item}`] = specsSalary[item] * specsQuantity[item];
+    }
 
-    Object.values(report).map(item => {
+    //add to list total salaries for team
+    Object.values(report).forEach(item => {
         sum += item;
-        Object.assign(report, { totalBudgetTeam: sum });
+        report["totalBudgetTeam"] = sum;
     });
 
     return report;
